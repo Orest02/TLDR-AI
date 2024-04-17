0ac7da440d809b707ddc801156479a758219f8f5
omalinovskyi <orest0206@gmail.com>
2024-04-17 09:58:35 +0200

    - delete n_batch

--- tldrai/modules/generation_pipeline/gguf_pipeline.py
+++ tldrai/modules/generation_pipeline/gguf_pipeline.py
@@ -3,7 +3,7 @@ from llama_cpp import Llama  # Import the Llama class from the llama-cpp-python 
 
 class GGUFModelLoader:
     def __init__(self, model_path, use_gpu=True, chat_format=None, n_ctx=16384, n_threads=8, n_gpu_layers=0,
-                 repo_id=None):
+                 repo_id=None, **kwargs):
         self.repo_id = repo_id
         self.model_path = model_path
         self.use_gpu = use_gpu
@@ -11,9 +11,9 @@ class GGUFModelLoader:
         self.n_ctx = n_ctx
         self.n_threads = n_threads
         self.n_gpu_layers = n_gpu_layers if self.use_gpu else 0
-        self.model = self.load_model()
+        self.model = self.load_model(**kwargs)
 
-    def load_model(self):
+    def load_model(self, **kwargs):
         # Initialize the Llama model with appropriate settings
         if self.repo_id is None:
             return Llama(
@@ -27,7 +27,12 @@ class GGUFModelLoader:
             return Llama.from_pretrained(
                 repo_id=self.repo_id,
                 filename=self.model_path,
-                verbose=False
+                n_ctx=self.n_ctx,
+                n_threads=self.n_threads,
+                n_gpu_layers=self.n_gpu_layers,
+                chat_format=self.chat_format,
+                verbose=False,
+                **kwargs,
             )
 
     def predict(self, prompt, max_tokens=512, stop=None, echo=False):
