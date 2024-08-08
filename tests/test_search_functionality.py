c9859de9e9610092dd2ee39c7a6796f42a79f7eb
omalinovskyi <orest0206@gmail.com>
2024-08-08 19:50:58 +0200

    - delete all pipelines but the ollama one - add info on forming a question to README.md - create a test for using stack search - add more StackOverflow params to the config - delete dependencies used in the deleted pipelines from pyproject.toml - create github action for testing a package installed from PyPi

---
+++ tests/test_search_functionality.py
@@ -0,0 +1,19 @@
+from click.testing import CliRunner
+from cli import main_cli
+
+
+def test_cli_integration():
+    runner = CliRunner()
+    result = runner.invoke(
+        main_cli,
+        [
+            "apply function to pandas column",
+            "-s",
+            "--set",
+            "summarization_pipeline.model=stable-code",
+        ],
+    )
+
+    assert result.exit_code == 0
+    # There's more than one word in the output
+    assert len(result.output.split()) > 1
